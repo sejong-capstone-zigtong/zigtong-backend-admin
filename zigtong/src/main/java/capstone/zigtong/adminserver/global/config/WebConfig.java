221f185776525e4cd330295053e1aa1ec8860596
youngcch <cyc8649@gmail.com>
2024-06-05 15:44:20 +0900

    update webconfig

--- zigtong/src/main/java/capstone/zigtong/adminserver/global/config/WebConfig.java
+++ zigtong/src/main/java/capstone/zigtong/adminserver/global/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://zigtong.vercel.app")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
